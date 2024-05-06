2f44050dca9534bf54337e9e84374383c58a5fdb
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-05-06 20:03:53 +0300

    add

---
+++ api/v1/views/index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    This is the index page handler for Flask.
+"""
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+classes = {"amenities": "Amenity",
+           "cities": "City",
+           "places": "Place",
+           "reviews": "Review",
+           "states": "State",
+           "users": "User"}
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    '''retrieves the number of each objects by type'''
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
